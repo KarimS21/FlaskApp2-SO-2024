ab37c14dfcf7720f96ee0c0c1e2bbcc320c81659
KarimS21 <112829652+KarimS21@users.noreply.github.com>
2024-06-15 02:15:01 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def addUser():
         print("Error al insertar los datos")
         return render_template("index.html")
 if __name__ == "__main__":
-    app.run(debug=True,port=8080)
+    app.run(host='0.0.0.0',debug=True,port=8080)
--- dockerfile
+++ dockerfile
@@ -5,7 +5,9 @@ WORKDIR /app
 COPY requirements.txt .
 COPY app.py .
 COPY conexion.py . 
+COPY Usuario.py .   
 COPY templates/ ./templates/  
+COPY static/ ./static/
 COPY .env . 
 
 RUN pip install -r requirements.txt
