18a836cd09452e13928ff0b565b17423c8524409
KarimS21 <112829652+KarimS21@users.noreply.github.com>
2024-06-16 22:56:34 -0500

    Add files via upload

--- conexion.py
+++ conexion.py
@@ -6,11 +6,11 @@ import bson
 class Conexion:
     def __init__(self):
         load_dotenv()
-        self.MONGO_URL=os.environ['MONGODB_URL'] #cambiar al 2 para que funcione con la base de datos de riva en su docker 
+        self.MONGO_URL=os.environ['MONGODB_URL2']
     def connectionDB(self):
         try:
             client = MongoClient(self.MONGO_URL)
-            db=client["Project-So-2024"]
+            db=client["SO-Project-2024"]
         except ConnectionError:
             print("Error de Conexion con la base de datos")
         return db
